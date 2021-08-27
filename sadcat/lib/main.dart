e9097e58b0f628ebf509c6a99471f1a9ca45bbb1
Jeerapas Kwansiri <jrps13k@gmail.com>
2021-08-27 20:17:55 +0700

    Add 4 pages with container, routes, action buttons, tabbar, listview

--- sadcat/lib/main.dart
+++ sadcat/lib/main.dart
@@ -11,10 +11,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
-        primarySwatch: Colors.brown,
+        primaryColor: Colors.amber,
+        accentColor: Colors.green,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.purple),
+        ),
       ),
-      home: MyHomePage(title: 'POPCAT 2.0'),
+      initialRoute: '/third',
+      routes: <String, WidgetBuilder>{
+        '/first' : (context) => FirstPage(),
+        '/second' : (context) => SecondPage(),
+        '/third' : (context) => ThirdPage(),
+        '/fourth' : (context) => FourthPage(),
+      },
     );
   }
 }
@@ -56,7 +65,17 @@ class _MyHomePageState extends State<MyHomePage> {
               'POPCAT',
             ),
 
-            catOnShow,
+            Container(
+              height: 150.0,
+              width: 150.0,
+              margin: EdgeInsets.only(bottom: 50.0),
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.blue.withOpacity(0.25),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: catOnShow,
+              ),
 
             Text(
               '$_counter',
@@ -82,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     }) 
                   }, 
                   child: Text('เพิ่ม'),
-                  )
+                  ),
               ],
             )
           ],
@@ -93,6 +112,164 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), 
+    );
+  }
+}
+
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Page'),
+        actions: [
+          IconButton(
+            onPressed: (){}, 
+            icon: Icon(Icons.fingerprint),
+            tooltip: 'Boutton 1',
+            ),
+          IconButton(
+            onPressed: (){}, 
+            icon: Icon(Icons.bungalow),
+            tooltip: 'Boutton 2',
+            ),
+        ],
+       ),
+       
+       body: Center(
+         child: Text('First Page'),
+       ),
+    );
+  }
+}
+
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+       ),
+       body: Center(
+         child: Table(
+           children: [
+             TableRow(
+               children: [
+                Container(
+                  margin: EdgeInsets.all(1.0),
+                  decoration: BoxDecoration(
+                    color: Colors.amber.shade300,
+                  ),
+                  child: Text('ID')),
+                Container(
+                  margin: EdgeInsets.all(1.0),
+                  decoration: BoxDecoration(
+                    color: Colors.amber.shade300,
+                  ),
+                  child: Text('Name')),
+                Container(
+                  margin: EdgeInsets.all(1.0),
+                  decoration: BoxDecoration(
+                    color: Colors.amber.shade300,
+                  ),
+                  child: Text('Gender')),
+               ],
+             ),
+             TableRow(
+               children: [
+                Text('1'),
+                Text('Harry'),
+                Text('Male'),
+               ],
+             ),
+             TableRow(
+               children: [
+                Text('2'),
+                Text('Hermione'),
+                Text('Female'),
+               ],
+             ),
+             TableRow(
+               children: [
+                Text('3'),
+                Text('Ron'),
+                Text('Male'),
+               ],
+             ),
+           ],
+         ),
+       ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Third Page'),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.cloud),
+              ),
+              Tab(
+                icon: Icon(Icons.shower),
+              ),
+              Tab(
+                icon: Icon(Icons.wb_sunny),
+              ),
+            ],
+          ),
+        ),
+         body: TabBarView(
+           children: [
+            Center(
+             child: Text('Cloudy'),
+            ),
+            Center(
+             child: Text('Rainy'),
+            ),
+            Center(
+             child: Text('Sunny'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class FourthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> entries = <String>['A','B','C','D','E','F','G','H','I'];
+    final List<int> colorCode = <int>[600, 500, 100];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fourth Page'),
+       ),
+       body: ListView.separated(
+         padding: EdgeInsets.all(8.0),
+         itemCount: entries.length,
+         itemBuilder: (context, index){
+           return Container(
+             height: 100.0,
+             color: Colors.amber[colorCode[index%3]],
+             child: Center(
+               child: Text('Enter ${entries[index]}'),
+             ),
+           );
+         },
+         separatorBuilder: (context, int) => Divider(),
+       ),
     );
   }
 }
